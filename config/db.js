e7bbf8497132c5c3f462302ef9370d76b3b3745c
Saeid Karimian <skarimian@gmail.com>
2019-10-27 04:09:06 +1100

    second commitment2

--- config/db.js
+++ config/db.js
@@ -1,16 +1,16 @@
-const mongoose = require('mongoose')
-const config = require('config')
+const mongoose = require("mongoose");
+const config = require("config");
 
-const db = config.get('mongoURI')
+const db = config.get("mongoURI");
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(db)
-        console.log('DB connected')
-    } catch (error) {
-        console.log(`Can not connect to db because ${error.message}`)
-        process.exit(1)
-    }
-}
+  try {
+    await mongoose.connect(db);
+    console.log("DB connected");
+  } catch (error) {
+    console.log(`Can not connect to db because ${error.message}`);
+    process.exit(1);
+  }
+};
 
-module.export = connectDB
+module.exports = connectDB;
