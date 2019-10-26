00f5257e1fed8bc96776f4ac69333f47571badd6
Saeid Karimian <skarimian@gmail.com>
2019-10-27 03:19:40 +1100

    Initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => res.send(`API Running`));
+
+const PORT = 5001; // this port for my developer profiles App
+
+app.listen(PORT, () =>
+  console.log("Server connected for developer profiles on port " + PORT)
+);
