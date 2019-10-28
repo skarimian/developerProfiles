fd947dbcf6a9a03455eee933af0f023ab039aabb
Saeid Karimian <skarimian@gmail.com>
2019-10-28 13:53:56 +1100

    client3

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,31 @@
-const express = require('express')
+const express = require("express");
+const { check, validationResult } = require("express-validator");
+
 const router = express.Router();
 
-// @route GET api/users
+// @route POST api/users
 // @desc User routes
 // @access Public
 
-router.get('/', (req,res) => res.send('Auth route'));
+router.get(
+  "/",
+  [
+    check("name", "Please enter a name")
+      .not()
+      .isEmpty(),
+    check("email", "Please enter a valid email").isEmail(),
+    check(
+      "password",
+      "Please enter a valid password with at least 6 charectors"
+    ).isLength({ min: 6 })
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors });
+    }
+    res.send("Auth route");
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,14 +4,14 @@ const connectDB = require("./config/db");
 const app = express();
 connectDB();
 
-
+// Initial middleware
+app.use(express.json({ extended: false }));
 
 //Define all API routes
-app.use('/api/users', require('./routes/api/users'));
-app.use('/api/profile', require('./routes/api/profile'));
-app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/posts', require('./routes/api/posts'));
-
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/posts", require("./routes/api/posts"));
 
 const PORT = 5001; // this port for my developer profiles App
 
